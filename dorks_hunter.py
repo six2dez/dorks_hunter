f054895d42402c3d44cbef2d34d41abaf4f68947
six2dez <six2dez@gmail.com>
2023-04-19 11:00:33 +0200

    googlesearch package fixes

--- dorks_hunter.py
+++ dorks_hunter.py
@@ -65,7 +65,7 @@ def main():
         if bool(inputs.output):
             save(inputs.output,description)
         try:
-            for results in search(dork, tld="com", lang="en", num=int(amount), start=0, stop=None, pause=2):
+            for results in search(dork, lang="en", num_results=int(amount)):
                 print (results)
                 time.sleep(0.1)
                 requ += 1
